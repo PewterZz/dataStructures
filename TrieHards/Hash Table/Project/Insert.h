ad7c307dc5a89e8772a5f27388d515458b2acfe5
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-06-20 08:23:37 +0700

    Add files via upload

---
+++ TrieHards/Hash Table/Project/Insert.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#ifndef InsertH
+#define InsertH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm2 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Word;
+	TLabel *Type;
+	TLabel *Definition;
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TEdit *Edit3;
+	TButton *Button1;
+	void __fastcall InsertionClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm2(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm2 *Form2;
+//---------------------------------------------------------------------------
+#endif
